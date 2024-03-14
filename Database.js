fc875ec6b546d00d20e4c720adabbc74e454b920
Nando <fmfernando61@gmail.com>
2024-03-14 17:53:16 -0300

    Finalizado database.js

--- Database.js
+++ Database.js
@@ -36,4 +36,46 @@ module.exports = class Database {
     const book = this.findBookByName(bookName)
     book?.removeFromStock(quantity)
   }
+
+  // Posters
+
+  findPosterByName(posterName){
+    return this.#storage.posters.find(p => p.name === posterName)
+  }
+
+  savePoster(poster){
+    const posterExists = this.findPosterByName(poster.name)
+    if(!posterExists){
+      this.#storage.posters.push(poster)
+    }
+  }
+
+  addPostersToStock(posterName, quantity){
+    const poster = this.findPosterByName(posterName)
+    poster?.addToStock(quantity)
+  }
+
+  removePostersFromStock(posterName, quantity){
+    const poster = this.findPosterByName(posterName)
+    poster?.removeFromStock(quantity)
+  }
+
+  saveUser(user){
+    const userExists = this.#storage.users.find(u => u.email === user.email)
+    if(!userExists){
+      this.#storage.users.push(user)
+    }
+  }
+
+  saveOrder(order){
+    this.#storage.orders.push(order)
+  }
+
+  showStorage(){
+    console.table(this.#storage.authors)
+    console.table(this.#storage.books)
+    console.table(this.#storage.posters)
+    console.table(this.#storage.users)
+    console.table(this.#storage.orders.map(order => order.data))
+  }
 }
